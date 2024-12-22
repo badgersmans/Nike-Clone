00748fd90fb02dd19a1ee3375f9e04a6306a09c2
Shawn Law <shawnlaw6669@gmail.com>
2024-12-22 16:41:55 +0800

    Initial Commit

---
+++ Nike Clone/Nike_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nike_CloneApp.swift
+//  Nike Clone
+//
+//  Created by Shawn Law on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nike_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
