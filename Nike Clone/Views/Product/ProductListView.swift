8e179db61632e7f2c9037cfef137b3d93c8ff48b
Shawn Law <shawnlaw6669@gmail.com>
2024-12-30 17:37:38 +0800

    abit of navigation

--- Nike Clone/Views/Product/ProductListView.swift
+++ Nike Clone/Views/Product/ProductListView.swift
@@ -12,21 +12,25 @@ struct ProductListView: View {
     @State private var numberListVM = NumberListViewModel()
     @State private var currentPage: Int = 1
     @State private var triggerId: Int? = nil // Track the ID that triggers shouldLoadData
-
+    
     let columns: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-
+    
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            LazyVGrid(columns: columns, spacing: 40) {
-                
-                ForEach(0..<20, id: \.self) { index in
-                    ProductItemView()
-                }
-            } // end of LazyVGrid
-        } // end of parent scrollview
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: columns, spacing: 40) {
+                    ForEach(0..<20, id: \.self) { index in
+                        NavigationLink(destination: HomeView()) {
+                            ProductItemView()
+                        }
+                        .buttonStyle(PlainButtonStyle())  // Disable the default link style
+                    }
+                } // end of LazyVGrid
+            } // end of parent scrollview
+        }
     }
 }
 
